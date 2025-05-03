b0a1beda768e2d05c4c0ca2ef644a46fe7ae8254
AtharvaSamant4 <atharvasamant43@gmail.com>
2025-05-03 12:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ import gdown
 
 MODEL_URLS = {
     "plant_vs_non_plant_model.h5": "https://drive.google.com/uc?id=18QcLMrAZq2Lbh3hlWEqXejL9cbS3kmXT",
-    "Plant_Classification_Architecture(Apple,Banana,etc).keras": "https://drive.google.com/uc?id=18tTN-huf5HvnJTRx7EitRKXheyyTeEwU
-",
-    "Plant_Disease_Predictor_with_Weather.keras": "https://drive.google.com/uc?id=1O_Ire7-TFwZ0QUj4ej6Rp2OwnmDaXXI1
-"
+    "Plant_Classification_Architecture(Apple,Banana,etc).keras": "https://drive.google.com/uc?id=18tTN-huf5HvnJTRx7EitRKXheyyTeEwU",
+    "Plant_Disease_Predictor_with_Weather.keras": "https://drive.google.com/uc?id=1O_Ire7-TFwZ0QUj4ej6Rp2OwnmDaXXI1"
 }
+
 
 for model_file, url in MODEL_URLS.items():
     if not os.path.exists(model_file):
